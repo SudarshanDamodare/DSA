b4adc79ee1c487c18ddb423c56167b718e0c0890
Sudarshan Damodare <sudarshan4172@gmail.com>
2021-10-13 03:51:04 +0530

    .

---
+++ gfg/dp/bstWithNKeys.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define us unordered_set
+#define um unordered_map
+
+// count no. of unique structural BSTs with n distinct keys
+int count(int n){
+  int dp[n+1]={0};
+  dp[0]=1;
+
+  for(int i=1;i<=n;i++){
+    for(int j=0;j<i;j++)
+      dp[i]+=dp[j]*dp[i-j-1];
+  }
+  return dp[n];
+}
+
+// this can be done using catalan Numbers formula i.e res(n) = (1/(n+1)) bino(2n,n) {2nCn}
+long double fact(int n){
+  long double res=1;
+  while(n)  res*=(n--);
+  return res;
+}
+
+long double bino(int n, int r){
+  long double res=1;
+  for(int i=0;i<r;i++)  res*=(n--);
+
+  return res/fact(r);
+}
+
+
+
+int main(){
+  #ifndef ONLINE_JUDGE
+    freopen("../input.txt","r",stdin);
+    freopen("../output.txt","w",stdout);
+  #endif
+
+  int n;cin>>n;
+  cout<<count(n)<<endl;
+  cout<<bino(2*n,n)/(n+1);
+  return 0;
+}
--- gfg/test.cpp
+++ gfg/test.cpp
@@ -1,41 +1,19 @@
 #include <bits/stdc++.h>
-#include<bits/stdc++.h>
 using namespace std;
-#define endl "\n"
-#define FAST_IO  ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
 #define ll long long int
 #define pb push_back
 #define mp make_pair
 #define us unordered_set
 #define um unordered_map
-ll mod=1e9+7;
 
-// ll dp[1000000000001];
 
-// void solve(){
-//   dp[1]=1;
-//   dp[2]=2;
-
-//   for(int i=3;i<1000;i++)  dp[i]=(dp[i-1]+dp[i-2])%mod;
-// }
 
 int main(){
-  FAST_IO;
   #ifndef ONLINE_JUDGE
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
   #endif
 
-  int t;cin>>t;
-  // solve();
-  while(t--){
-    ll n; cin>>n;
-    ll dp[n+1];
-    dp[1]=1, dp[2]=2;
-    cout<<n<<" ";
-    for(int i=3;i<=n;i++)  dp[i]=((dp[i-1]%mod)+(dp[i-2]%mod))%mod;
-    cout<<dp[n]<<endl;
-  }
-
+  cout<<"Fine\n";
   return 0;
 }
