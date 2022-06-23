fd2ae902cbbc3a1100d417881afcfe0db275ec84
Sudarshan <sudarshan4172@gmail.com>
2022-06-24 02:53:59 +0530

    .

--- gfg/test.cpp
+++ gfg/test.cpp
@@ -5,28 +5,9 @@ using namespace std;
 #define mkp make_pair
 #define us unordered_set
 #define um unordered_map
+#define all(x) x.begin(),x.end()
 
-int lcm=1,mini=INT_MAX;
 
-
-void sieve(int n, vector<bool> &isPrime){
-  if(n<=1) return;
-
-  for(int i=2;i*i<=n;i++){
-    if(isPrime[i]){
-      for(int j=i*i;j<=n;j+=i) isPrime[j]=false;
-    }
-  }
-}
-
-int gcd(int a, int b){
-  if(b==0)  return a;
-  else  return gcd(b,a%b);
-}
-
-int getLCM(int a, int b){
-  return (a*b)/gcd(a,b);
-}
 
 int main(){
   #ifndef ONLINE_JUDGE
@@ -34,28 +15,6 @@ int main(){
     freopen("output.txt","w",stdout);
   #endif
 
-  int n=0,a;
-  vector<int> v;
-  lcm=1,mini=INT_MAX;
-
-  while(cin>>a){
-    v.pb(a);
-    n++;
-    mini=min(a,mini);
-  }
-
-
-  for(auto it:v){
-    if(it!=mini){
-      lcm=getLCM(lcm,it);
-    }
-  }
-
-  vector<bool> isPrime(lcm+mini+1,1);
-
-  sieve(lcm+mini, isPrime);
-  if(isPrime[lcm+mini]) cout<<lcm+mini<<endl;
-  else  cout<<"None\n";
 
   return 0;
 }
